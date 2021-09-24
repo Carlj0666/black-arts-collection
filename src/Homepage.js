7d42b3bcfdb043f258511d6aa282af1ab270c4ba
Carlj0666 <carlj1881@yahoo.com>
2021-09-23 20:25:30 -0700

    experimenting with toggle

--- src/Homepage.js
+++ src/Homepage.js
@@ -11,32 +11,53 @@ import React from 'react';
 import './App.css';
 
 class Homepage extends React.Component{
-
-    state = {
-        color: 'Red',
-        nightOn: true
-    }
+    constructor(props) {
+        super(props);
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.handleOffClick = this.handleOffClick.bind(this);
+        this.state = {
+            isNightOn: false,
+            color: "red"
+        }
+    };
 
     handleClick = (e) => {
 
-        const nightTheme = this.state.nightOn
+        const nightTheme = this.state.nightOn;
 
         this.setState({
             nightOn: !nightTheme
         })
+    };
+
+    handleOnClick() {
+        this.setState({
+            isNightOn: true,
+            color: "Red"
+        });
+    }
+
+    handleOffClick() {
+        this.setState({
+            isNightOn: false,
+            color: "Red"
+        });
     }
 
     render(){
-
+        const isNightOn = this.state.nightOn;
+        let button;
+        if (isNightOn) {
+            button = <button className="nightButtonOn" onClick={this.handleOnClick}>Night night theme {this.state.nightOn ? "On" : "Off"}</button>;
+        } else {
+            button = <button className="nightButtonOff" onClick={this.handleOffClick}>Night night theme {this.state.nightOn ? "On" : "Off"}</button>;
+        }
 
         return(
             <>
-            
             <div style={{color: this.state.color}}>
                 <h1> Welcome to the Black Arts Collection </h1>
-                <button className="nightThemeButton" onClick={this.handleClick}>Night night theme {this.state.nightOn ? "On" : "Off"}</button>
-                {/* <h1>Welcome to {this.props.color} Arts Home</h1>
-                <h4>A place where {this.props.alignment} Arts are cultivated</h4> */}
+                {button}
             </div>
             </>
         )
