3757511661b5aec2cc0424872d2435869490d2d5
Carlj0666 <carlj1881@yahoo.com>
2021-09-23 17:13:31 -0700

    jic save

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Homepage from './Homepage';
 
 function App() {  
 
-  const atribs = [
-    {id: 1, color: 'Black', alignment: 'Good'},
-    {id: 2, color: 'Green', alignment: 'Neutral'},
-    {id: 3, color: 'White', alignment: 'Evil'},
-  ]
+  // const atribs = [
+  //   {id: 1, color: 'Black', alignment: 'Good'},
+  //   {id: 2, color: 'Green', alignment: 'Neutral'},
+  //   {id: 3, color: 'White', alignment: 'Evil'},
+  // ]
 
-  const namedAtribs = atribs.map(a => <Homepage key={a.id} color={a.color} alignment={a.alignment}/>)
+  // const namedAtribs = atribs.map(a => <Homepage key={a.id} color={a.color} alignment={a.alignment}/>)
 
   return (
     <div className='App'>
       {/* map name logic extracted to the namedAtribs const */}
-      {namedAtribs}
+      {/* {namedAtribs} */}
+      <Homepage />
     </div>
   );
 }
--- src/Homepage.js
+++ src/Homepage.js
@@ -13,16 +13,32 @@ import './App.css';
 class Homepage extends React.Component{
 
     state = {
-        color: 'red',
-        on: true
+        color: 'Red',
+        nightOn: true
+    }
+
+    handleClick = (e) => {
+
+        const nightTheme = this.state.nightOn
+
+        this.setState({
+            nightOn: !nightTheme
+        })
     }
 
     render(){
+
+
         return(
+            <>
+            
             <div style={{color: this.state.color}}>
-                <h1>Welcome to {this.props.color} Arts Home</h1>
-                <h4>A place where {this.props.alignment} Arts are cultivated</h4>
+                <h1> Welcome to the Black Arts Collection </h1>
+                <button className="nightThemeButton" onClick={this.handleClick}>Night night theme {this.state.nightOn ? "On" : "Off"}</button>
+                {/* <h1>Welcome to {this.props.color} Arts Home</h1>
+                <h4>A place where {this.props.alignment} Arts are cultivated</h4> */}
             </div>
+            </>
         )
     }
 
