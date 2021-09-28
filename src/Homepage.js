e2de7c8623cd9652ba497d3e642a0bcda0b89487
Carlj0666 <carlj1881@yahoo.com>
2021-09-27 17:43:04 -0700

    working on rails as api backend and testing

--- src/Homepage.js
+++ src/Homepage.js
@@ -23,7 +23,7 @@ class Homepage extends React.Component{
 
         this.setState({
             on: !nightButton
-        })
+        }, () => console.log())
     }
 
     render(){
@@ -33,8 +33,6 @@ class Homepage extends React.Component{
             <div style={{color: this.state.color}}>
                 <h1> Welcome to the Black Arts Collection </h1>
                 <button onClick={this.handleClick}>Button {this.state.on ? "On" : "Off"}</button>
-                {/* <h1>Welcome to {this.props.color} Arts Home</h1>
-                <h4>A place where {this.props.alignment} Arts are cultivated</h4> */}
             </div>
             </>
         )
